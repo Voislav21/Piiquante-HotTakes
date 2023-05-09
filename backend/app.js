5bdda96b1de3f0ea56bc4ff6f91f4a7193643d8c
Voislav Ivanovski <voislav21@gmail.com>
2023-05-09 16:20:56 +0200

    install mongodb and connect app to mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
+
+//MONGODB PW: bC6a2n2mSy3NGYqv //
+//MONGODB CONNECTION: mongodb+srv://voislav21:<password>@cluster0.b6e5igi.mongodb.net/?retryWrites=true&w=majority //
+
+// Import express and mongoose //
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to database //
+mongoose.connect('mongodb+srv://voislav21:bC6a2n2mSy3NGYqv@cluster0.b6e5igi.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Connected successfully to mongoDB Atlas!');
+    })
+    .catch((error) => {
+        console.log('Unable to connect to mongoDB Atlas!');
+        console.error(error);
+    });
 
 app.use((req, res, next) => {
   console.log('Request received!');
