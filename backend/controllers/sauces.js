f90520c136137f10a056b4181ce49e00e63192cf
Voislav Ivanovski <voislav21@gmail.com>
2023-05-10 16:38:06 +0200

    modify sauce function created

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,6 +1,7 @@
 // Sauces related bussiness mogic //
 
 // Import sauce model //
+const { deserialize } = require('v8');
 const Sauce = require('../models/sauces');
 // Node file system package //
 const fs = require('fs');
@@ -41,3 +42,45 @@ exports.getOneSauce = (req, res, next) => {
         });
     });
 };
+
+exports.modifySauce = (req, res, next) => {
+    let sauce = new Sauce({ _id: req.params._id });
+    if (req.file) {
+        const url = req.protocol + '://' + req.get('host');
+        req.body.sauce = JSON.parse(req.body.sauce);
+        sauce = {
+            _id: req.params.id,
+            userId: req.body.sauce.userId,
+            manufacturer: req.body.sauce.manufacturer,
+            description: req.body.sauce.description,
+            mainPepper: req.body.sauce.mainPepper,
+            imageUrl: url + '/images/' + req.file.filename,
+            heat: req.body.sauce.heat,
+            likes: req.body.sauce.likes,
+            dislikes: req.body.sauce.dislikes
+        };
+    } else {
+        sauce = {
+            _id: req.params.id,
+            userId: req.body.userId,
+            manufacturer: req.body.manufacturer,
+            description: req.body.description,
+            mainPepper: req.body.mainPepper,
+            imageUrl: req.body.imageUrl,
+            heat: req.body.heat,
+            likes: req.body.likes,
+            dislikes: req.body.dislikes
+        };
+    }
+        Sauce.updateOne({ _id: req.params.id }, sauce)
+        .then(() => {
+            res.status(201).json({
+                message: 'Sauce updated successfully!'
+            });
+        }).catch((error) => {
+            res.status(400).json({
+                error: error
+            });
+        });
+};
+
