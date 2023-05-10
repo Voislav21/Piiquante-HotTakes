3a150b1699a62b88da3b018160477e3e10a8e112
Voislav Ivanovski <voislav21@gmail.com>
2023-05-10 16:45:29 +0200

    Get all sauces function created

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -102,3 +102,14 @@ exports.deleteSauce = (req, res, next) => {
         });
     });
 };
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+    .then((sauces) => {
+        res.status(200).json(sauces);
+    }).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    });
+};
