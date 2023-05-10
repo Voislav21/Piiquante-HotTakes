e680a39b48242da8e2713aa4c035e962bbf28e1d
Voislav Ivanovski <voislav21@gmail.com>
2023-05-10 16:24:51 +0200

    create sauce function controller created

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,32 @@
+// Sauces related bussiness mogic //
+
+// Import sauce model //
+const Sauce = require('../models/sauces');
+// Node file system package //
+const fs = require('fs');
+
+// Create function //
+exports.createSauce = (req, res, next) => {
+    const url = req.protocol + '://' + req.get('host');
+    req.body.sauce = JSON.parse(req.body.sauce);
+    const sauce = new Sauce({
+        userId: req.body.sauce.userId,
+        manufacturer: req.body.sauce.manufacturer,
+        description: req.body.sauce.description,
+        mainPepper: req.body.sauce.mainPepper,
+        imageUrl: url + '/images/' + req.file.filename,
+        heat: req.body.sauce.heat,
+        likes: req.body.sauce.likes,
+        dislikes: req.body.sauce.dislikes
+    });
+    sauce.save()
+    .then(() => {
+        res.status(201).json({
+            message: 'Post saved successfully!'
+        });
+    }).catch((error) => {
+        res.status(400).json({
+            error: error
+        });
+    });
+};
