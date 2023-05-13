dee09618877f37a779f8e70fe8e2f7f8c471dead
Voislav Ivanovski <voislav21@gmail.com>
2023-05-13 19:06:43 +0200

    Post request route  added to like or dislike a sauce

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -16,6 +16,7 @@ router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.post('/:id/like', auth, sauceCtrl.likesOrDislikes);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
 // Export module //
