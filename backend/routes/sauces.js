4981d30e1efa0d662a30f847b1246826b72cfe61
Voislav Ivanovski <voislav21@gmail.com>
2023-05-13 14:22:07 +0200

    Comments added

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -4,15 +4,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Import middleware //
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
+// Import sauce controllers //
 const sauceCtrl = require('../controllers/sauces');
 
+// Send requests //
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 
+// Export module //
 module.exports = router;
