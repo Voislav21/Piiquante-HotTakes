6483622babdb2ab5e59b09c7f33929121383dda2
Voislav Ivanovski <voislav21@gmail.com>
2023-05-15 15:58:10 +0200

    fixed typofrom require to required

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose');
 // create sauce schema //
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
-    name: { type: String, require: true },
-    manufacturer: { type: String, require: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
     description: { type: String, required: true },
     mainPepper: { type: String, required: true },
-    imageUrl: { type: String, require: true },
-    heat: { type: Number, require: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
     likes: { type: Number, default: 0 },
     dislikes: { type: Number, default: 0 },
     usersLiked: { type: Array, default: []},
